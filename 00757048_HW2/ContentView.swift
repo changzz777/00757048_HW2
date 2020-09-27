5e2be7e96efb236f2998d63a4670440e52faca1e
saltychess <37352173@qq.com>
2020-09-27 17:27:44 +0800

    Initial Commit

---
+++ 00757048_HW2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757048_HW2
+//
+//  Created by User08 on 2020/9/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757048_HW2/_0757048_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757048_HW2App.swift
+//  00757048_HW2
+//
+//  Created by User08 on 2020/9/27.
+//
+
+import SwiftUI
+
+@main
+struct _0757048_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
